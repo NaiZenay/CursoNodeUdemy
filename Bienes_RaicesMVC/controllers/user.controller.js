48458fe97509afcda2b2f9a83ee19269dfe500a4
Ian <115850010+NaiZenay@users.noreply.github.com>
2024-09-18 16:05:21 -0600

    Agregando validaciones de formulario de registro

--- Bienes_RaicesMVC/controllers/user.controller.js
+++ Bienes_RaicesMVC/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js"
+import { check,validationResult } from "express-validator"
 
 const formLogin = (req, res) => {
     res.render('auth/login', {
@@ -13,6 +14,22 @@ const registerForm = (req, res) => {
 }
 
 const register = async(req, res) => {
+    //validaciones
+    await check("nombre").notEmpty().withMessage("EL nombre esta vacio").run(req)
+    await check("email").isEmail().withMessage("Eso no es un email").run(req)
+    await check("password").isLength({min:6}).withMessage("El password debe ser de al menos 6 caracteres").run(req)
+    await check("repetir-password").equals("password").withMessage("Los passwords no coinciden").run(req)
+
+    let result=validationResult(req)
+    //verificar user vacio
+    if(result.isEmpty()){
+        return res.render("auth/register",{
+            pagina:"Crea Cuenta",
+            errores:result.array()
+        })
+    }
+    
+
     const user=await User.create(req.body);
     res.json=user
 }
